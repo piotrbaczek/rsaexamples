a33b241226d59e0da23f24700846ff8526965a16
Dziadek <Dziadek>
2016-02-25 13:58:01 +0100

    code structure + added rc2

--- 3des/enc_dec.php
+++ 3des/enc_dec.php
@@ -5,22 +5,41 @@ include '../phpseclib/phpseclib/Crypt/DES.php';
 include '../phpseclib/phpseclib/Crypt/TripleDES.php';
 
 $plaintext = 'Something very secret.';
-$password = '#$2123Bsa#3Qzfk231';
+$password = 'VdcpDTWTc5Aehxgv2uL9haaFddDBhrc8uCMG3ykg';
 
 echo 'Plaintext: ' . $plaintext . "\r\n";
 
+//Create new TripleDES object
 $tripledes_encode = new phpseclib\Crypt\TripleDES(phpseclib\Crypt\TripleDES::MODE_ECB);
+//Set keylength to 256
 $tripledes_encode->setKeyLength(256);
-$tripledes_encode->setPassword($password, 'pbkdf2', 'sha512');
+//Set OPENSSL as preferred engine
+$tripledes_encode->setPreferredEngine(phpseclib\Crypt\TripleDES::ENGINE_OPENSSL);
+//set password with pbkdf2 and sha512 as hashing method
+$tripledes_encode->setPassword($password, 'pbkdf2', 'sha512', NULL, 4096);
 
-$ciphertext = base64_encode($tripledes_encode->encrypt($plaintext));
+//Encrypt the plaintext
+$ciphertext_raw = $tripledes_encode->encrypt($plaintext);
 
-echo 'Ciphertext: ' . $ciphertext . "\r\n";
+echo 'Ciphertext (RAW): ' . $ciphertext_raw . "\r\n";
 
+$ciphertext = base64_encode($ciphertext_raw);
+
+echo 'Ciphertext (base64): ' . $ciphertext . "\r\n";
+
+//Create new TripleDES Object
 $tripledes_decode = new phpseclib\Crypt\TripleDES(phpseclib\Crypt\TripleDES::MODE_ECB);
+//Set keylength to 256
 $tripledes_decode->setKeyLength(256);
-$tripledes_decode->setPassword($password, 'pbkdf2', 'sha512');
+//Set OPENSSL as preferred engine
+$tripledes_decode->setPreferredEngine(phpseclib\Crypt\TripleDES::ENGINE_OPENSSL);
+//set password with pbkdf2 and sha512 as hashing method
+$tripledes_decode->setPassword($password, 'pbkdf2', 'sha512', NULL, 4096);
 
+//Decode from base64 and decrypt
 $decrypted = $tripledes_decode->decrypt(base64_decode($ciphertext));
 
 echo 'Decrypted: ' . $decrypted . "\r\n";
+
+//is everything ok?
+var_dump($plaintext == $decrypted);
--- rc2/enc_dec.php
+++ rc2/enc_dec.php
@@ -2,23 +2,23 @@
 
 include '../phpseclib/phpseclib/Crypt/Base.php';
 include '../phpseclib/phpseclib/Crypt/DES.php';
-include '../phpseclib/phpseclib/Crypt/RC4.php';
+include '../phpseclib/phpseclib/Crypt/RC2.php';
 
 $plaintext = 'Something very secret.';
 $password = 'VdcpDTWTc5Aehxgv2uL9haaFddDBhrc8uCMG3ykg';
 
 echo 'Plaintext: ' . $plaintext . "\r\n";
 
-//Create new RC4 object for encrypting
-$des_encrypt = new \phpseclib\Crypt\RC4(\phpseclib\Crypt\RC4::MODE_ECB);
+//Create new RC2 object for encrypting
+$rc2_encrypt = new \phpseclib\Crypt\RC2(\phpseclib\Crypt\RC2::MODE_ECB);
 //set OPENSSL as preferred engine
-$des_encrypt->setPreferredEngine(phpseclib\Crypt\RC4::ENGINE_OPENSSL);
+$rc2_encrypt->setPreferredEngine(phpseclib\Crypt\RC2::ENGINE_OPENSSL);
 //set keylength to 256
-$des_encrypt->setKeyLength(256);
+$rc2_encrypt->setKeyLength(256);
 //set pbkdf2 with sha512 and 4096 iterations as password hashing method
-$des_encrypt->setPassword($password, 'pbkdf2', 'sha512', NULL, 4096);
+$rc2_encrypt->setPassword($password, 'pbkdf2', 'sha512', NULL, 4096);
 
-$ciphertext_raw = $des_encrypt->encrypt($plaintext);
+$ciphertext_raw = $rc2_encrypt->encrypt($plaintext);
 
 echo 'Ciphertext(RAW): ' . $ciphertext_raw . "\r\n";
 
@@ -26,16 +26,16 @@ $ciphertext = base64_encode($ciphertext_raw);
 
 echo 'Ciphertext(base64): ' . $ciphertext . "\r\n";
 
-//Create new RC4 object for decryption
-$des_decrypt = new phpseclib\Crypt\RC4(phpseclib\Crypt\RC4::MODE_ECB);
+//Create new RC2 object for decryption
+$rc2_decrypt = new phpseclib\Crypt\RC2(\phpseclib\Crypt\RC2::MODE_ECB);
 //set OPENSSL as preferred engine
-$des_decrypt->setPreferredEngine(phpseclib\Crypt\RC4::ENGINE_OPENSSL);
+$rc2_decrypt->setPreferredEngine(phpseclib\Crypt\RC2::ENGINE_OPENSSL);
 //set key length to 256
-$des_decrypt->setKeyLength(256);
+$rc2_decrypt->setKeyLength(256);
 //set pbkdf2 with sha512 and 4096 iterations as password hashing method
-$des_decrypt->setPassword($password, 'pbkdf2', 'sha512', NULL, 4096);
+$rc2_decrypt->setPassword($password, 'pbkdf2', 'sha512', NULL, 4096);
 //Decode from base64 and decrypt
-$decrypted = $des_decrypt->decrypt(base64_decode($ciphertext));
+$decrypted = $rc2_decrypt->decrypt(base64_decode($ciphertext));
 
 echo 'Decrypted: ' . $decrypted . "\r\n";
 
