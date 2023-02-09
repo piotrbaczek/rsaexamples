0e3c68ea9d80b908ce7207bd22bdee0b34170760
Piotr Bączek <pgolasz@gmail.com>
2023-02-09 19:19:51 +0100

    Encryption with info

--- src/Console/EncryptionInfo.php
+++ src/Console/EncryptionInfo.php
@@ -2,14 +2,48 @@
 
 namespace piotrbaczek\rsaexamples\Console;
 
+use phpseclib3\Math\BigInteger;
+use piotrbaczek\rsaexamples\rsa\Common\RsaWrapper;
+use piotrbaczek\rsaexamples\rsa\KeyGenerator;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class EncryptionInfo extends Command
 {
+    protected static $defaultDescription = 'Encryption example';
+
+    protected function configure()
+    {
+        $this->addArgument('message', InputArgument::REQUIRED);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        var_dump($input);
+        $privateKeyInfo = new \piotrbaczek\rsaexamples\rsa\PrivateKeyInfo(new RsaWrapper());
+
+        $keysPath = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'keys');
+
+        $privateKeyInfo->loadKey($keysPath . DIRECTORY_SEPARATOR . 'private.pem',
+            KeyGenerator::MY_PRIVATE_KEY_PASSWORD);
+
+        $publicKey = $privateKeyInfo->getKey()->getPublicKey();
+
+        $cipherText = $publicKey->encrypt($input->getArgument('message'));
+
+
+        $output->writeln(' RAW Ciphertext:');
+        $output->writeln($cipherText);
+        $output->writeln('');
+
+        $cipherTextInDecimal = new BigInteger($cipherText, 256);
+
+        $output->writeln('Ciphertext in decimal: ' . $cipherTextInDecimal->toString());
+
+        $decryptedMessage = $privateKeyInfo->getKey()->decrypt($cipherText);
+        $output->writeln('Decrypted message: ' . $decryptedMessage);
+
+        return 1;
     }
 }
