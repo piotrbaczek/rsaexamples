8639296903489be087f56454985a4df81c004177
Piotr Bączek <pgolasz@gmail.com>
2023-04-06 23:52:05 +0200

    AES functions

---
+++ src/Aes/Common/AesWrapper.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace piotrbaczek\rsaexamples\Aes\Common;
+
+use Exception;
+use phpseclib3\Crypt\AES;
+
+class AesWrapper implements AesInterface
+{
+    /** @var AES $aes */
+    private $aes;
+
+    public function __construct(AES $aes)
+    {
+        $this->aes = $aes;
+    }
+
+    public function setKey(string $key): AesInterface
+    {
+        $this->aes->setKey($key);
+
+        return $this;
+    }
+
+    public function setKeyLength(int $length): AesInterface
+    {
+        $this->aes->setKeyLength($length);
+
+        return $this;
+    }
+
+    /**
+     * @param string $message
+     * @return string
+     * @throws Exception
+     */
+    public function encrypt(string $message): string
+    {
+        $this->aes->setIV(random_bytes(16));
+        return $this->aes->encrypt($message);
+    }
+
+    public function decrypt(string $cipherText): string
+    {
+        return $this->aes->decrypt($cipherText);
+    }
+}
